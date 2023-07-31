63a6f53701d900fae161e7e45948133b5e23e0e7
Laysson <laysson.ens@gmail.com>
2023-07-31 00:07:11 -0300

    Separate Postgres and SQLite connections into different files

--- config/database/postgres.go
+++ config/database/postgres.go
@@ -1,1 +1,31 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/LayssonENS/go-FastHTTP-api/config"
+	"log"
+)
+
+func NewPostgresConnection() (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		config.GetEnv().DbConfig.Host,
+		config.GetEnv().DbConfig.Port,
+		config.GetEnv().DbConfig.User,
+		config.GetEnv().DbConfig.Password,
+		config.GetEnv().DbConfig.Name,
+	)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping database: %v", err)
+	}
+
+	log.Println("Successfully connected to the PostgreSQL database.")
+	return db, nil
+}
--- config/database/sqlite.go
+++ config/database/sqlite.go
@@ -21,26 +21,3 @@ func NewSqliteConnection() (*sql.DB, error) {
 	log.Println("Successfully connected to the SQLite database.")
 	return db, nil
 }
-
-func NewPostgresConnection() (*sql.DB, error) {
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		config.GetEnv().DbConfig.Host,
-		config.GetEnv().DbConfig.Port,
-		config.GetEnv().DbConfig.User,
-		config.GetEnv().DbConfig.Password,
-		config.GetEnv().DbConfig.Name,
-	)
-
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open database: %v", err)
-	}
-
-	err = db.Ping()
-	if err != nil {
-		return nil, fmt.Errorf("failed to ping database: %v", err)
-	}
-
-	log.Println("Successfully connected to the PostgreSQL database.")
-	return db, nil
-}
