8673fc6b2d42c25dffe01a555c4f7fc6c17d3123
Laysson <laysson.ens@gmail.com>
2023-07-31 00:06:20 -0300

    Implemented dynamic database connection setup

--- app/main.go
+++ app/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	http "github.com/LayssonENS/go-FastHTTP-api/user/delivery/http"
+	"database/sql"
+	"github.com/LayssonENS/go-FastHTTP-api/config"
+	"github.com/LayssonENS/go-FastHTTP-api/config/database"
+	"github.com/LayssonENS/go-FastHTTP-api/user/delivery/http"
 	userRepo "github.com/LayssonENS/go-FastHTTP-api/user/repository"
 	userUsecase "github.com/LayssonENS/go-FastHTTP-api/user/usecase"
 	"github.com/buaazp/fasthttprouter"
@@ -10,14 +13,29 @@ import (
 )
 
 func main() {
+	var db *sql.DB
+	var err error
 
-	userRepository := userRepo.NewUserRepository(nil)
-	uUseCase := userUsecase.NewUserUseCase(userRepository, 10)
+	switch config.GetEnv().DbConfig.DbType {
+	case "postgres":
+		db, err = database.NewPostgresConnection()
+	case "sqlite":
+		db, err = database.NewSqliteConnection()
+	default:
+		log.Fatalf("Tipo de banco de dados desconhecido: %s", config.GetEnv().DbConfig.DbType)
+	}
+
+	if err != nil {
+		log.Fatalf("Falha ao se conectar ao banco de dados: %v", err)
+	}
+
+	userRepository := userRepo.NewUserRepository(db)
+	userUseCase := userUsecase.NewUserUseCase(userRepository, 10)
 
 	router := fasthttprouter.New()
-	http.NewUserHandler(router, uUseCase)
+	http.NewUserHandler(router, userUseCase)
 
-	err := fasthttp.ListenAndServe(":8080", router.Handler)
+	err = fasthttp.ListenAndServe(":8080", router.Handler)
 	if err != nil {
 		log.Fatalln(err)
 		return
--- go.mod
+++ go.mod
@@ -2,11 +2,14 @@ module github.com/LayssonENS/go-FastHTTP-api
 
 go 1.20
 
-require github.com/buaazp/fasthttprouter v0.1.1
+require (
+	github.com/Netflix/go-env v0.0.0-20220526054621-78278af1949d
+	github.com/buaazp/fasthttprouter v0.1.1
+	github.com/valyala/fasthttp v1.48.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/klauspost/compress v1.16.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.48.0 // indirect
 )
