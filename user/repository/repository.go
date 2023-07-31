352d9d68aa595c42dc87b769ca8d4df027652393
Laysson <laysson.ens@gmail.com>
2023-07-30 21:25:04 -0300

    Added UserRepository with GetByID method

--- user/repository/repository.go
+++ user/repository/repository.go
@@ -1,1 +1,34 @@
 package repository
+
+import (
+	"database/sql"
+	"github.com/LayssonENS/go-FastHTTP-api/domain"
+	"log"
+)
+
+type userRepository struct {
+	Conn *sql.DB
+}
+
+func NewUserRepository(conn *sql.DB) domain.UserRepository {
+	return &userRepository{conn}
+}
+
+func (u *userRepository) GetByID(id int64) (domain.User, error) {
+	db, err := sql.Open("postgres", "user=DB_USER password=DB_PASSWORD dbname=DB_NAME sslmode=disable")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer db.Close()
+
+	row := db.QueryRow("SELECT id, name FROM users WHERE id = $1", id)
+
+	user := new(domain.User)
+	err = row.Scan(&user.ID, &user.Name)
+	if err != nil {
+		return domain.User{}, err
+	}
+
+	return *user, nil
+}
