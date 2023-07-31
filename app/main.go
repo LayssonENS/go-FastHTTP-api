faa3c21c56f8451b92c61a044152d6264f72efb9
Laysson <laysson.ens@gmail.com>
2023-07-30 21:34:23 -0300

    Add user use case implementation in main

--- app/main.go
+++ app/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	http "github.com/LayssonENS/go-FastHTTP-api/user/delivery/http"
+	userRepo "github.com/LayssonENS/go-FastHTTP-api/user/repository"
+	userUsecase "github.com/LayssonENS/go-FastHTTP-api/user/usecase"
+	"github.com/buaazp/fasthttprouter"
+	"github.com/valyala/fasthttp"
+)
+
+func main() {
+
+	userRepository := userRepo.NewUserRepository(nil)
+	uUseCase := userUsecase.NewUserUseCase(userRepository, 10)
+
+	router := fasthttprouter.New()
+	http.NewUserHandler(router, uUseCase)
+
+	fasthttp.ListenAndServe(":8080", router.Handler)
+}
