b3f1a28a177fa6f0cb24ae2da4d9f8c8f6a90228
Laysson <laysson.ens@gmail.com>
2023-07-30 21:29:15 -0300

    Add user use case implementation structure

---
+++ user/usecase/user_ucase.go
@@ -0,0 +1,23 @@
+package usecase
+
+import (
+	"github.com/LayssonENS/go-FastHTTP-api/domain"
+	"time"
+)
+
+type userUseCase struct {
+	userRepo       domain.UserRepository
+	contextTimeout time.Duration
+}
+
+func NewUserUseCase(a domain.UserUseCase, timeout time.Duration) domain.UserUseCase {
+	return &userUseCase{
+		userRepo:       a,
+		contextTimeout: timeout,
+	}
+}
+
+func (u *userUseCase) GetByID(id int64) (domain.User, error) {
+	//TODO implement me
+	panic("implement me")
+}
