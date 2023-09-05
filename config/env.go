458ef0534a2967db9f5d59b0f9af5c728d6c76c2
Laysson <laysson.ens@gmail.com>
2023-09-04 22:12:14 -0300

    Update default DB_HOST in environment settings

--- config/env.go
+++ config/env.go
@@ -17,7 +17,7 @@ type DbConfig struct {
 	User     string `env:"DB_USER,default=postgres"`
 	Port     string `env:"DB_PORT,default=5432"`
 	Password string `env:"DB_PASSWORD,default=postgres"`
-	Host     string `env:"DB_HOST,default=postgres"`
+	Host     string `env:"DB_HOST,default=localhost"`
 	Name     string `env:"DB_NAME,default=postgres"`
 	Path     string `env:"DB_PATH,default=./sqlite.db"`
 	DbType   string `env:"DB_TYPE,default=postgres"`
