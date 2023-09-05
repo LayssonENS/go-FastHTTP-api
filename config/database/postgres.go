8df41574bfd9f8b78ca36a572aa20342804c73aa
Laysson <laysson.ens@gmail.com>
2023-09-04 21:57:09 -0300

    Refactor PostgreSQL connection string construction

--- config/database/postgres.go
+++ config/database/postgres.go
@@ -6,18 +6,20 @@ import (
 	"log"
 
 	"github.com/LayssonENS/go-FastHTTP-api/config"
+	_ "github.com/lib/pq"
 )
 
 func NewPostgresConnection() (*sql.DB, error) {
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		config.GetEnv().DbConfig.Host,
-		config.GetEnv().DbConfig.Port,
+	dataSourceName := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
 		config.GetEnv().DbConfig.User,
 		config.GetEnv().DbConfig.Password,
+		config.GetEnv().DbConfig.Host,
+		config.GetEnv().DbConfig.Port,
 		config.GetEnv().DbConfig.Name,
 	)
 
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := sql.Open("postgres", dataSourceName)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open database: %v", err)
 	}
