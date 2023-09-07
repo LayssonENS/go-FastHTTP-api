5b2487645d823a4ed30fc53320fbdc6491b06339
Laysson <laysson.ens@gmail.com>
2023-09-06 22:09:53 -0300

    Implement actual DB query in GetByID and Add CreateUser method

--- user/repository/repository.go
+++ user/repository/repository.go
@@ -1,7 +1,10 @@
 package repository
 
 import (
+	"context"
 	"database/sql"
+	"errors"
+	"fmt"
 
 	"github.com/LayssonENS/go-FastHTTP-api/domain"
 )
@@ -15,12 +18,31 @@ func NewUserRepository(conn *sql.DB) domain.UserRepository {
 }
 
 func (u *userRepository) GetByID(id int64) (domain.User, error) {
+	var user domain.User
 
-	return domain.User{
-		ID:        0,
-		Name:      "Test",
-		CreatedAt: "test",
-		UpdatedAt: "test",
-	}, nil
+	query := `SELECT id, name FROM users WHERE id = $1`
+	err := u.Conn.QueryRow(query, id).Scan(&user.ID, &user.Name)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return domain.User{}, fmt.Errorf("user with ID %d not found", id)
+		}
+		return domain.User{}, err
+	}
 
+	return user, nil
+}
+
+func (u *userRepository) CreateUser(user *domain.User) error {
+	query := `
+		INSERT INTO users (name)
+		VALUES ($1)
+		RETURNING id;
+	`
+
+	err := u.Conn.QueryRowContext(context.Background(), query, user.Name).Scan(&user.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
